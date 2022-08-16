225cca7c14caad49494ce1c9e9503ff9c430f002
Jayesh Goyal <jayeshgoyal28@gmail.com>
2022-08-16 23:03:34 +0530

    Async code fixed

--- server.js
+++ server.js
@@ -1,12 +1,60 @@
+//importin packages
 const express = require('express');
-const app = express();
-
+const https = require('https');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
+
+//constants
+const appid = "f1ec8910be37f5f35898902c0268f95e"
+const units = "metric";
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 const port = process.env.PORT || 8000;
 
+
+//creating server
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(__dirname+"/index.html")
+})
+
+app.post('/home',(req,res) => {
+
+    console.log(req.body.cityNames);
+    let city_names = req.body.cityNames;
+    let url_list = [];
+    city_names.forEach((city_name)=>{
+        
+        const url = baseURL+ city_name +"&appid="+ appid +"&units="+units;
+        url_list.push(url);
+          
+    })
+    let axios_get_array = [];
+    url_list.forEach((URL)=>{
+      axios_get_array.push(axios.get(URL));
+    })
+
+    axios
+    .all(axios_get_array)
+    .then(
+      axios.spread((...responses) => {
+        let result = "";
+        responses.forEach((resp)=>{
+            console.log(resp.data);
+            result += "<h1>The temprature in "+ resp.data.name +" is " +resp.data.main.temp +"C. <h1>"
+        })
+        city_names = [];
+        res.send(result);
+      })
+    )
+    .catch(errors => {
+      console.error(errors);
+    });    
+
+    
 })
 
 app.listen(port, () => {
