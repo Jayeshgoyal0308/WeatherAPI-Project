9478e03bc34104113039262fee0319d2ec82e7eb
Jayesh Goyal <jayeshgoyal28@gmail.com>
2022-08-16 19:08:30 +0530

    Initial Server SetUp

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`app is running at ${port}....`);
+});
